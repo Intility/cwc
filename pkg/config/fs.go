e3ccac08bc8c1bcfdbae4badc73e3251c6f843fe
Emil Kjelsrud <emil.kjelsrud@intility.no>
2024-04-06 11:39:59 +0200

    fix(sec): do not report variable file reading as a security risk

--- pkg/config/fs.go
+++ pkg/config/fs.go
@@ -8,7 +8,18 @@ import (
 type OSFileManager struct{}
 
 func (y *OSFileManager) Read(path string) ([]byte, error) {
-	data, err := os.ReadFile(path)
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("file does not exist: %w", err)
+		}
+
+		return nil, fmt.Errorf("error stating file: %w", err)
+	}
+
+	// even though the path is a variable, it is safe to assume that the file exists
+	// in a safe location as we are using XDG Base Directory Specification
+	data, err := os.ReadFile(path) // #nosec
 	if err != nil {
 		return nil, fmt.Errorf("error reading file: %w", err)
 	}
